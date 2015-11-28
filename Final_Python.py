80b78d628c5fa0b33b9d2e2d7ee2da8c39b41be8
lomaxvogtm <mcl9@alfred.edu>
2015-11-28 17:16:40 -0500

    Webpage

---
+++ Final_Python.py
@@ -0,0 +1,97 @@
+__author__ = 'Madeleine'
+import tkinter as tk
+
+
+class Elevator():
+
+    def __init__(self, canvas, num_floors, width, height, vspace, hspace):
+        self.num_floors = num_floors
+        self.width = width
+        self.height = height
+        self.vspace = vspace
+        self.hspace = hspace
+        self.canvas = canvas
+        self.position = 0
+        self.passengers = []
+        self.list = []
+        self.root = root
+
+    def draw_elevator(self):
+        self.canvas.delete("all")
+        for i in range(self.num_floors):
+            if self.position <= 3:
+                if (self.num_floors-i-1) == self.position:
+                    self.canvas.create_rectangle(self.hspace, self.vspace+i*(self.height+self.vspace),
+                                                 self.hspace+self.width, (i+1)*(self.height+self.vspace),
+                                                 fill='red', outline='black')
+                else:
+                    self.canvas.create_rectangle(self.hspace, self.vspace+i*(self.height+self.vspace),
+                                                 self.hspace+self.width, (i+1)*(self.height+self.vspace),
+                                                 fill='grey', outline='black')
+
+
+class Building:
+    width = 20
+    height = 30
+    hspace = 10
+    vspace = 5
+    num_floors = 4
+
+    def __init__(self, root):
+        self.list_pass = []
+        self.root = root
+        self.direction = 1
+        self.canvas = tk.Canvas(root, height=(Building.num_floors*Building.height) +
+                                             ((Building.num_floors+1)*Building.vspace), width=(2*Building.hspace) +
+                                                                                              Building.width, bg='grey')
+        self.canvas.grid(row=0, column=0)
+        outline = tk.Frame(root)
+        outline.grid(row=0, column=1)
+        self.elevator = Elevator(self.canvas, Building.num_floors, Building.width,
+                                 Building.height, Building.vspace, Building.hspace)
+        self.elevator.passengers = []
+        for i in range(Building.num_floors):
+            self.elevator.passengers.append(tk.StringVar())
+            entry = tk.Entry(outline, textvariable=self.elevator.passengers[i])
+            entry.grid(row=i, column=1)
+        self.controller()
+
+    def controller(self):
+        self.elevator.draw_elevator()
+        if self.elevator.position == Building.num_floors-1:
+            self.direction = -1
+        elif self.elevator.position == 0:
+            self.direction = 1
+        # Get passengers from current floor
+        self.elevator.position += self.direction
+        x = self.elevator.passengers[self.elevator.position].get()
+        self.elevator.passengers[self.elevator.position].set("")
+        x = x.split(',')
+        print(x)
+        self.root.after(2000, self.controller)
+
+        def move_elevator(self):
+            for passenger in x:
+                try:
+                    floor = int(passenger)-1
+                    if floor > Building.num_floors-1:
+                        x.remove(passenger)
+                    if floor < 0:
+                        x.remove(passenger)
+                    if floor == self.elevator.position:
+                        x.remove(passenger)
+                    if self.direction == -1 and floor - self.elevator.position < self.elevator.position:
+                        self.list_pass.append(floor)
+                    if self.direction == 1 and floor - self.elevator.position > self.elevator.position:
+                        self.list_pass.append(floor)
+                except ValueError:
+                    pass
+
+
+root = tk.Tk()
+draw = Building(root)
+root.mainloop()
+
+#cosmetic stuff
+#passengers on elevator list
+#delay/error when picking up passengers
